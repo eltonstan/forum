73dc6fafb2c31bb892338a1ce3f2d5bffc22d7d2
Elton Standerski <eltonstan@yahoo.com.br>
2020-12-17 16:34:55 -0300

    Aula 01 a 03: profiles, roles e testes

--- src/main/java/br/com/alura/forum/config/security/DevSecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/DevSecurityConfigurations.java
@@ -19,8 +19,8 @@ import br.com.alura.forum.repository.UsuarioRepository;
 
 @EnableWebSecurity
 @Configuration
-@Profile("prod")
-public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+@Profile(value = {"dev","test"})
+public class DevSecurityConfigurations extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	AutenticacaoService autenticacaoService;
@@ -45,13 +45,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-		.antMatchers(HttpMethod.GET, "/topicos").permitAll()
-		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
-		.antMatchers(HttpMethod.GET, "/actuator").permitAll()
-		.antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
-		.antMatchers(HttpMethod.POST, "/auth").permitAll()
-		.antMatchers(HttpMethod.DELETE, "/topicos/*").hasRole("MODERADOR")
-		.anyRequest().authenticated()
+		.antMatchers(HttpMethod.GET, "/**").permitAll()
 		.and().csrf().disable()
 		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 		.and().addFilterBefore(new AutenticacaoViaTokenFilter(tokenService, usuarioRepository), UsernamePasswordAuthenticationFilter.class);
